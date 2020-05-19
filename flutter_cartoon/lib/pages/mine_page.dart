dcdfc1ebff1767b48274817b6feee2367d2ffb33
cuixuebin <cuixuebin@ihealthlabs.com.cn>
2020-05-19 20:18:32 +0800

    我的页面编写

--- flutter_cartoon/lib/pages/mine_page.dart
+++ flutter_cartoon/lib/pages/mine_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttercartoon/widget/SliverCustomHeader.dart';
 class MinePage extends StatefulWidget {
   @override
   _MinePageState createState() => _MinePageState();
@@ -7,7 +9,115 @@ class MinePage extends StatefulWidget {
 class _MinePageState extends State<MinePage> {
   @override
   Widget build(BuildContext context) {
-    return
-      Center(child: Container(child: Text('我的'),));
+    return Scaffold(
+      body: MainPage(),
+    );
   }
 }
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverPersistentHeader(
+             pinned: false,//标题栏是否固定
+            delegate: SliverCustomHeaderDelegate(
+              title: '',
+              collapsedHeight: 40,
+              expandedHeight: 300,
+              paddingTop: MediaQuery.of(context).padding.top,
+                fileImageUrl: 'assets/images/mine/mine_bg_for_girl@2x.png'
+            ),
+//             floating: true,
+//            snap: false,
+//            expandedHeight: 230,
+//              flexibleSpace:Container(child: Image.asset('assets/images/mine/mine_bg_for_girl@2x.png',fit: BoxFit.fill,height: 230,),height: 230,)
+          ),
+          SliverList(
+            // itemExtent: 50.0,
+              delegate: SliverChildBuilderDelegate((context,index){
+                return MineRow("assets/images/mine/mine_vip@2x.png","我的VIP",true);
+              }, childCount:20)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: Container(height: 12,)
+          ),
+        ),
+          SliverList(
+             // itemExtent: 50.0,
+              delegate: SliverChildBuilderDelegate((context,index){
+            return Container(color: Colors.red,height: 30,);
+          }, childCount:20)
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Container(height: 12,)
+            ),
+          ),
+          SliverList(
+            // itemExtent: 50.0,
+              delegate: SliverChildBuilderDelegate((context,index){
+                return Container(color: Colors.red,height: 30,);
+              }, childCount:20)
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Container(height: 12,)
+            ),
+          ),
+          SliverList(
+            // itemExtent: 50.0,
+              delegate: SliverChildBuilderDelegate((context,index){
+                return Container(color: Colors.red,height: 30,);
+              }, childCount:20)
+          ),
+
+        ],
+      ),
+    );
+  }
+
+}
+
+class MineRow extends StatelessWidget {
+  String imagefile;
+  String titleString;
+  bool showLine = true;
+  MineRow(this.imagefile,this.titleString,this.showLine);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          Row(
+           // crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+
+              Padding(child: Image.asset(imagefile,width: 32,height: 32,),padding: EdgeInsets.only(right: 20),),
+              Expanded(
+                  child: Text(titleString)
+              ),Icon(Icons.chevron_right)
+              ],),
+          Divider(height: 10,indent:42)
+            ],
+          ),
+
+      );
+
+  }
+}
+
+
---
+++ flutter_cartoon/lib/widget/SliverCustomHeader.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
+  final double collapsedHeight;
+  final double expandedHeight;
+  final double paddingTop;
+  final String coverImgUrl;
+  final String fileImageUrl;
+  final String title;
+  String statusBarMode = 'dark';
+
+  SliverCustomHeaderDelegate({
+    this.collapsedHeight,
+    this.expandedHeight,
+    this.paddingTop,
+    this.coverImgUrl,
+    this.fileImageUrl,
+    this.title,
+  });
+
+  @override
+  double get minExtent => this.collapsedHeight + this.paddingTop;
+
+  @override
+  double get maxExtent => this.expandedHeight;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+
+  void updateStatusBarBrightness(shrinkOffset) {
+    if(shrinkOffset > 50 && this.statusBarMode == 'dark') {
+      this.statusBarMode = 'light';
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.light,
+      ));
+    } else if(shrinkOffset <= 50 && this.statusBarMode == 'light') {
+      this.statusBarMode = 'dark';
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.dark,
+      ));
+    }
+  }
+
+  Color makeStickyHeaderBgColor(shrinkOffset) {
+    final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+    return Color.fromARGB(alpha, 255, 255, 255);
+  }
+
+  Color makeStickyHeaderTextColor(shrinkOffset, isIcon) {
+    if(shrinkOffset <= 50) {
+      return isIcon ? Colors.white : Colors.transparent;
+    } else {
+      final int alpha = (shrinkOffset / (this.maxExtent - this.minExtent) * 255).clamp(0, 255).toInt();
+      return Color.fromARGB(alpha, 0, 0, 0);
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    this.updateStatusBarBrightness(shrinkOffset);
+    return Container(
+      height: this.maxExtent,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(child: this.coverImgUrl != null ? Image.network(this.coverImgUrl, fit: BoxFit.cover) :Image.asset(this.fileImageUrl,fit: BoxFit.cover,)),
+          Positioned(
+            left: 0,
+            top: this.maxExtent / 2,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0x00000000),
+                    Color(0x90000000),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              color: this.makeStickyHeaderBgColor(shrinkOffset),
+              child: SafeArea(
+                bottom: false,
+                child: Container(
+                  height: this.collapsedHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          color: this.makeStickyHeaderTextColor(shrinkOffset, true),
+                        ),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      Text(
+                        this.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: this.makeStickyHeaderTextColor(shrinkOffset, false),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.share,
+                          color: this.makeStickyHeaderTextColor(shrinkOffset, true),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
